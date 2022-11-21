1b1f5e7989adfcafd85116350fdd1577e51d639b
Krystian Gronek <k.gronek3@student.uw.edu.pl>
2022-11-21 15:52:23 +0100

    dodano kody do szacowania rozkladu dwumianowego ujemnego

--- scripts/analiza_NB.R
+++ scripts/analiza_NB.R
@@ -12,6 +12,14 @@ policja <- read.csv(file = "./data/policja.csv", sep = ";") %>%
     mutate_at("date", as.Date, format = "%Y-%m-%d")
 
 x = policja$wypadki
+
+# Dla rozkładu ujemnego dwumianowego będziemy szacować parametry dla 
+# dziesiątek wypadków ponieważ w zbiorze danych występują wartości
+# dla których funkcja wiarygodności przyjmuje wartości nieskończoności
+# (z winy funkcji w R)
+
+x = x/10
+x = round(x)
 N = length(x)
 
 #### Szacowanie metodą największej wiarygodności ####
@@ -19,18 +27,36 @@ lnL_NB <- function(parametry) {
     r = parametry[1]
     p = parametry[2]
     ll = sum(log(gamma(x + r))) + sum(log_sum(x)) - N * log(gamma(r)) + 
-        sum(x)*log(1-p) + N * r * log(p)
+        sum(x) * log(1 - p) + N * r * log(p)
+}
+
+lnL_NB <- function(parametry) {
+    r = parametry[1]
+    p = parametry[2]
+    ll = sum(log(gamma(x + r))) + sum(log(factorial(x))) - N * log(gamma(r)) + 
+        sum(x) * log(1 - p) + N * r * log(p)
 }
 
 gradient = function(parametry) {
     r = parametry[1]
     p = parametry[2]
     gr = rep(0, times = 2)
-    gr[1] = -sum(x)/(1-p) + N * r / p
-    gr[2] = sum(digamma(x+r)) - N * digamma(r) + N * log(p)
-    
+    gr[1] = - sum(x) / (1 - p) + N * r / p
+    gr[2] = sum(digamma(x + r)) - N * digamma(r) + N * log(p)
     return(gr)
 }
+
+hessian = function(parametry) {
+    r = parametry[1]
+    p = parametry[2]
+    hes = matrix(0, nrow = 2, ncol = 2)
+    hes[1,1] = - sum(x) / (1 - p) ^ 2 + N * r / p ^ 2
+    hes[1,2] = N / p
+    hes[2,1] = N / p
+    hes[2,2] = sum(trigamma(x + r)) - N * trigamma(r)
+    return(hes)
+}
+
 
 # Wynik bez gradientu i hesjanu
 # błąd
@@ -38,9 +64,8 @@ wynik = maxNR(lnL_NB, start = c(0.5,0.5))
 summary(wynik)
 
 # Wynik z gradientem i hesjanem
-
-curve(lnL_NB, 0 , 200, col = "black")
-abline(v = 82.6067, col= "red")
+wynik = maxNR(lnL_NB, grad = gradient, hess = hessian, start = c(0.5,0.5))
+summary(wynik)
 
 # sprawdzic w help maxNR "tolerancja"
 
@@ -53,19 +78,3 @@ ggplot(data = policja) +
 wynik = maxNR(lnL3, start = c(0.5,0.5))
 summary(wynik)
 
-# WORK IN PROGRESS
-# Hesjan?
-hessian = function(parametry) {
-    r = parametry[1]
-    p = parametry[2]
-    hes = matrix(0, nrow = 2, ncol = 2)
-    hes[1,1] = sum(x)/((1 - p) ^ 2) - N * r / p ^ 2
-    hes[1,2] = N / p
-    hes[2,1] = N / p
-    hes[2,2] = sum(trigamma(x+r)) - N * trigamma(r)
-    
-    return(hes)
-}
-
-# Rozwiązanie: liczyć model jako dziesiątki wypadków a nie liczba wypadków 
-# (ale tylko dla rozkładu ujemnego dwumianowego)
